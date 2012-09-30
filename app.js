d9ad9774002cb80ce217edc6a1b3f6d3df7972db
fnobi <shin@fnobi.com>
2012-10-01 02:45:40 +0900

    埋め込みのcssとjsを分けるところまでできた。あとはライブラリ。

--- app.js
+++ app.js
@@ -10,19 +10,24 @@ commander
 	.option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
 	.parse(process.argv);
 
+
+// 変数宣言
 var jsGetIt;
 var args = commander.args;
 
+// 引数無しならエラー終了
 if (!args[0]) {
 	console.error("enter username and codename.");
 	process.exit(1);
 }
 
 if (args[0].match(/^([^\/]+)\/+([^\/]+)$/)) {
+	// 引数1が user/code のカタチになっているなら、そのcodeをとる
 	jsGetIt = new JSGetIt(RegExp.$1, RegExp.$2);
-	args.shift();
 } else if (args.length >= 2) {
+	// 引数が user code のカタチになっているなら、そのcodeをとる
 	jsGetIt = new JSGetIt(args.shift(), args.shift());
 }
 
-jsGetIt.download(args.shift());
+// ダウンロード
+jsGetIt.download();
